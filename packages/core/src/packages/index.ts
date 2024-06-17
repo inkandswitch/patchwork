4066fe8b3c553c6b2df20209f135bad9437ba704
Geoffrey Litt <gklitt@gmail.com>
2024-06-17 16:22:44 -0400

    comment out kanban for now

--- packages/core/src/packages/index.ts
+++ packages/core/src/packages/index.ts
@@ -2,7 +2,7 @@ export * as bot from "./bot";
 export * as datagrid from "./datagrid";
 export * as essay from "./essay";
 export * as folder from "./folder";
-export * as kanban from "./kanban";
+// export * as kanban from "./kanban";
 export * as pkg from "./pkg";
 export * as tldraw from "./tldraw";
 export * as ambSheet from "./ambsheet";
