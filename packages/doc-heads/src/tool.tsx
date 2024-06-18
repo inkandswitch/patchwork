711c58cc3ee776f711b00d40ca8989f7efabf994
Geoffrey Litt <gklitt@gmail.com>
2024-06-18 17:26:32 -0400

    copy heads to clipboard

--- packages/doc-heads/src/tool.tsx
+++ packages/doc-heads/src/tool.tsx
@@ -1,14 +1,23 @@
 import { type EditorProps, type Tool, hashToColor } from "@patchwork/sdk";
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import * as Automerge from "@automerge/automerge";
+import { useCallback } from "react";
 import styles from "./doc-heads.module.css";
 
 export const DocHeadsViewer = ({ docUrl }: EditorProps<unknown, never>) => {
   const [doc] = useDocument<unknown>(docUrl);
   const heads = Automerge.getHeads(doc);
   const color = hashToColor(heads[0]);
+
+  const copyToClipboard = useCallback(() => {
+    navigator.clipboard.writeText(heads.join(", "));
+  }, [heads]);
+
   return (
-    <div className={`${styles.docHeadsViewer}`}>
+    <div
+      className={`${styles.docHeadsViewer} cursor-pointer`}
+      onClick={copyToClipboard}
+    >
       <div className="flex items-center">
         <div
           className="w-4 h-4 rounded-full"
