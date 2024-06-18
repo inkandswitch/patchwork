5c1f074d886b6505494c4e60d6124dc953ef1b24
Geoffrey Litt <gklitt@gmail.com>
2024-06-18 16:58:07 -0400

    crude approach to scoping styles with css modules

--- packages/doc-heads/src/tool.tsx
+++ packages/doc-heads/src/tool.tsx
@@ -1,19 +1,21 @@
 import { type EditorProps, type Tool, hashToColor } from "@patchwork/sdk";
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import * as Automerge from "@automerge/automerge";
-import "./index.css";
+import styles from "./doc-heads.module.css";
 
 export const DocHeadsViewer = ({ docUrl }: EditorProps<unknown, never>) => {
   const [doc] = useDocument<unknown>(docUrl);
   const heads = Automerge.getHeads(doc);
   const color = hashToColor(heads[0]);
   return (
-    <div className="flex items-center">
-      <div
-        className="w-4 h-4 rounded-full"
-        style={{ backgroundColor: color }}
-      ></div>
-      <span className="ml-2 font-mono">{heads[0]?.slice(0, 6)}</span>
+    <div className={`${styles.docHeadsViewer}`}>
+      <div className="flex items-center">
+        <div
+          className="w-4 h-4 rounded-full"
+          style={{ backgroundColor: color }}
+        ></div>
+        <span className="ml-2 font-mono">{heads[0]?.slice(0, 6)}</span>
+      </div>
     </div>
   );
 };
