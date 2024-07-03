e0c6b011eceb75ee4e022f506b85becd4fc92134
Geoffrey Litt <gklitt@gmail.com>
2024-07-02 21:41:37 -0400

    make build list look nicer

--- packages/jacquard/src/tool.tsx
+++ packages/jacquard/src/tool.tsx
@@ -22,16 +22,52 @@ export const JacquardProject = ({
   }
 
   return (
-    <div className="h-full overflow-auto">
+    <div className="h-full overflow-auto flex flex-col gap-4">
       {doc.buildRuns
         .slice()
         .reverse()
         .map((run, index) => (
-          <li key={index} className="p-2">
-            {run.outputs.map(({ path }) => path).join(",")} generated at{" "}
-            {new Date(run.timestamp).toLocaleString()} by
+          <div
+            key={index}
+            className="p-3 border-b border-gray-300 flex flex-col gap-1"
+          >
+            <div className="text-xs text-gray-500">
+              {new Date(run.timestamp).toLocaleString([], {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </div>
             <pre>{run.command}</pre>
-          </li>
+            <div className="cursor-default">
+              <div className="text-sm font-medium">Inputs:</div>
+              <div className="flex flex-wrap gap-2 text-xs">
+                {run.inputs.map(({ path }) => (
+                  <div
+                    key={path}
+                    className="px-2 py-1 bg-gray-100 border border-gray-300 rounded-full"
+                  >
+                    {path}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="cursor-default">
+              <div className="text-sm font-medium">Outputs:</div>
+              <div className="flex flex-wrap gap-2 text-xs">
+                {run.outputs.map(({ path }) => (
+                  <div
+                    key={path}
+                    className="px-2 py-1 bg-gray-100 border border-gray-300 rounded-full"
+                  >
+                    {path}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         ))}
     </div>
   );
