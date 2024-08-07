8114ab59353a33c69e1386c85ea3841778551b6c
Joshua Horowitz <joshuah@alum.mit.edu>
2024-08-07 00:14:46 -0700

    fix hook error in usePackageModulesInRootFolder

--- os/src/packages/pkg/usePackages.ts
+++ os/src/packages/pkg/usePackages.ts
@@ -23,6 +23,11 @@ export const usePackageModulesInRootFolder = (): Package[] => {
     return NO_PACKAGES; // return same array so hooks that depend on usePackageModulesInRootFolder don't create an infinite loops
   }
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return usePackageModulesInRootFolderForReal();
+};
+
+const usePackageModulesInRootFolderForReal = (): Package[] => {
   const folderDocWithMetadata = useRootFolderDocWithChildren();
   const flatDocLinks = folderDocWithMetadata?.flatDocLinks;
   const [modules, setModules] = useState<Package[]>([]);
