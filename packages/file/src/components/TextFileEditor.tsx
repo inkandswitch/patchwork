cf792ded7b56cb3c69ff47d600fb28061b5a947b
Joshua Horowitz <joshuah@alum.mit.edu>
2024-08-09 00:10:33 -0700

    new CodeMirror component which makes me feel a lot better

--- packages/file/src/components/TextFileEditor.tsx
+++ packages/file/src/components/TextFileEditor.tsx
@@ -20,6 +20,7 @@ import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import { Cursor } from "@automerge/automerge/next";
 import { json } from "@codemirror/lang-json";
 import { python } from "@codemirror/lang-python";
+import { Extension } from "@codemirror/state";
 import { keymap, lineNumbers } from "@codemirror/view";
 import clsx from "clsx";
 import { EditorView, basicSetup } from "codemirror";
@@ -27,6 +28,7 @@ import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 import { tool } from "..";
 import { selectedAnchorsPlugin } from "../../../essay/src/codemirrorPlugins/setSelectedAnchors";
 import { FileDoc, TextFileContent } from "../datatype";
+import { CodeMirror } from "./CodeMirror";
 
 export type TextFileDoc = FileDoc & {
   content: TextFileContent;
@@ -45,7 +47,13 @@ export const TextFileEditor = ({
   getFakeDocPathForDocUrl,
   mainDocUrl,
 }: EditorProps<TextAnchor, string>) => {
-  const [container, setContainer] = useState<HTMLElement | null>(null);
+  // TODO: Only reason we need containerRef is for scrollObserverPlugin.
+  // Ideally, scrollObserverPlugin would use DOM information from CodeMirror
+  // which is available to it directly. But we're actually not scrolling in
+  // CodeMirror itself; we're scrolling a container div. We might not want to do
+  // this! I think it's better to let CodeMirror handle scrolling, cuz it's
+  // better at it. Anyway, for now I'm just using this.
+  const containerRef = useRef<HTMLDivElement>(null);
   const [editor, setEditor] = useState<EditorView>();
   const [fileDoc] = useDocument<TextFileDoc>(docUrl);
   const handle = useHandleDef<TextFileDoc>(docUrl);
@@ -85,78 +93,69 @@ export const TextFileEditor = ({
   // TODO: this obviously sucks
   const changeToolUIStateRef = useRefForCallback(changeToolUIState);
 
-  const [didInitializeEditor, setDidInitializeEditor] = useState(false);
+  const stableExtensions: Extension = useMemo(() => {
+    return [
+      suppressModEnter, // keep on top to take priority, or be classier someday
+      basicSetup,
+      automergeSyncPlugin({
+        handle,
+        path: pathToText,
+      }),
+      annotationsPlugin,
+      EditorView.lineWrapping,
+      lineNumbers(),
+      scrollObserverPlugin({ containerRef, fileDocRef, changeToolUIStateRef }),
+    ];
+  }, [changeToolUIStateRef, fileDocRef, handle]);
 
-  useEffect(() => {
-    if (didInitializeEditor || !container || !toolUIState || !fileDoc) {
-      return;
+  const docDependentExtensions = useMemo(() => {
+    if (!fileDoc) {
+      return [];
     }
 
-    const suppressModEnter = keymap.of([
-      {
-        key: "Mod-Enter",
-        preventDefault: true,
-        run: () => true,
-      },
-    ]);
+    return [
+      getPluginsByType(fileDoc.type),
+      ...(setSelectedAnchors
+        ? [
+            selectedAnchorsPlugin({
+              setSelectedAnchors,
+              annotationsRef,
+              doc: fileDoc,
+              path: pathToText,
+            }),
+          ]
+        : []),
+    ];
+  }, [fileDoc, setSelectedAnchors]);
 
-    const doc = handle.docSync();
-    const view = new EditorView({
-      doc: doc!.content.value, // TODO: JAH strict fix
-      extensions: [
-        suppressModEnter, // keep on top to take priority, or be classier someday
-        basicSetup,
-        automergeSyncPlugin({
-          handle,
-          path: pathToText,
-        }),
-        getPluginsByType(doc!.type), // TODO: JAH strict fix
-        annotationsPlugin,
-        EditorView.lineWrapping,
-        lineNumbers(),
-        ...(setSelectedAnchors && doc
-          ? [
-              selectedAnchorsPlugin({
-                setSelectedAnchors,
-                annotationsRef,
-                doc,
-                path: pathToText,
-              }),
-            ]
-          : []),
-        scrollObserverPlugin({ container, fileDocRef, changeToolUIStateRef }),
-      ],
-      parent: container,
-      scrollTo: scrollTo({ toolUIState, fileDoc }),
-    });
+  const allExtensions = useMemo(() => {
+    return [...stableExtensions, ...docDependentExtensions];
+  }, [stableExtensions, docDependentExtensions]);
 
-    view.focus();
-    setEditor(view);
-
-    setDidInitializeEditor(true);
-
-    // TODO: this is a single-use component, so no destroying the editor!
-  }, [
-    changeToolUIState,
-    changeToolUIStateRef,
-    container,
-    didInitializeEditor,
-    fileDoc,
-    fileDocRef,
-    handle,
-    setSelectedAnchors,
-    toolUIState,
-  ]);
+  useEffect(() => {
+    // TODO: cursor will likely be off-screen if we have persisted scroll
+    // position! we should persist cursor position!
+    editor?.focus();
+  }, [editor]);
 
   return (
-    <div
-      className={clsx("codemirror-editor h-full overflow-auto", {
-        // Scroll position initialization should occur without an animation,
-        // then we set scroll-smooth for future scrolls
-        "scroll-smooth": didInitializeEditor,
-      })}
-      ref={setContainer}
-    />
+    fileDoc &&
+    toolUIState && (
+      <CodeMirror
+        ref={containerRef}
+        setEditorView={setEditor}
+        initialDoc={fileDoc.content.value}
+        extensions={allExtensions}
+        editorViewConfig={{
+          scrollTo: scrollTo({ toolUIState, fileDoc }),
+        }}
+        className={clsx("codemirror-editor h-full overflow-auto", {
+          // Scroll position initialization should occur without an animation,
+          // then we set scroll-smooth for future scrolls
+          "scroll-smooth": !!editor,
+        })}
+      />
+    )
   );
 };
 
@@ -194,20 +193,33 @@ const scrollTo = ({
   }
 };
 
+const suppressModEnter = keymap.of([
+  {
+    key: "Mod-Enter",
+    preventDefault: true,
+    run: () => true,
+  },
+]);
+
 /** Plugin for writing scroll position to UI state */
 const scrollObserverPlugin = ({
-  container,
+  containerRef,
   fileDocRef,
   changeToolUIStateRef,
 }: {
-  container: HTMLElement;
-  fileDocRef: React.MutableRefObject<TextFileDoc | undefined>;
+  containerRef: MutableRefObject<HTMLElement | null>;
+  fileDocRef: MutableRefObject<TextFileDoc | undefined>;
   changeToolUIStateRef: MutableRefObject<(fn: (state: any) => void) => void>;
 }) => {
   let writeScrollTimeout: number | undefined = undefined;
 
   return EditorView.domEventObservers({
     scroll: (_event, view) => {
+      const container = containerRef.current;
+      if (!container) {
+        console.warn("TextFileEditor scroll: No container");
+        return;
+      }
       if (writeScrollTimeout) {
         window.clearTimeout(writeScrollTimeout);
       }
