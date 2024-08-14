e9354f74b6b53f051a4b99eacecbfd0e4e09efbb
Joshua Horowitz <joshuah@alum.mit.edu>
2024-08-14 13:24:35 -0700

    engraft: minimal editor, no doc connection yet

--- packages/engraft/src/components/EngraftEditor.tsx
+++ packages/engraft/src/components/EngraftEditor.tsx
@@ -1,5 +1,30 @@
 import { EditorProps } from "@/tools";
+import { EngraftDoc } from "../datatype";
+import { useDocument } from "@automerge/automerge-repo-react-hooks";
+import { ToolWithView } from "@engraft/hostkit";
+import { makeFancyContext } from "@engraft/fancy-setup";
+import { useState } from "react";
+import { ToolProgram } from "@engraft/hostkit";
+
+// TODO
+const context = makeFancyContext();
+
+const noOp = () => {};
+const empty = {};
 
 export const EngraftEditor = (props: EditorProps<unknown, unknown>) => {
-  return <div>EngraftEditor</div>;
+  const [program, updateProgram] = useState<ToolProgram>(() =>
+    context.makeSlotWithCode("")
+  );
+
+  return (
+    <ToolWithView
+      program={program}
+      updateProgram={updateProgram}
+      reportOutputState={noOp}
+      varBindings={empty}
+      expand={true}
+      context={context}
+    />
+  );
 };
