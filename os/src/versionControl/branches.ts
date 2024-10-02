512b6e29d7490a1dd17e5ae0990e19bd11756cb4
Joshua Horowitz <joshuah@alum.mit.edu>
2024-10-02 16:26:18 -0700

    fix tsc error from #51

--- os/src/versionControl/branches.ts
+++ os/src/versionControl/branches.ts
@@ -330,7 +330,9 @@ export const hasLegacyBranchesToMigrate = async ({
   // this is gross - we need to re-fetch the version control metadata doc
   // because this can get called multiple times with stale data.
   const latestBranchScopeVersionControlMetadata =
-    branchScopeAndActiveBranchInfo.branchScopeVersionControlMetadataOm?.handle.docSync();
+    branchScopeAndActiveBranchInfo.branchScopeVersionControlMetadataOm?.handle.docSync() as
+      | A.Doc<VersionControlSidecarDoc & { isBranchScope: true }>
+      | undefined;
 
   // We only migrate if the doc has legacy branches and there are no "current" branches.
   // This is conservative but should cover most cases.
