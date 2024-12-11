5fb69514cc2895e17af14b3b4ecf61a9db935de7
Geoffrey Litt <gklitt@gmail.com>
2024-12-11 16:11:39 -0500

    make it scrollable

--- packages/amb-poker/src/tool.tsx
+++ packages/amb-poker/src/tool.tsx
@@ -84,116 +84,122 @@ export const AmbPoker: React.FC<EditorProps<AmbPokerDoc, string>> = ({
   }
 
   return (
-    <div className="flex flex-col h-full">
-      <div className="p-4 border-b">
-        <h2 className="text-lg font-semibold mb-2">Model</h2>
-        <div className="flex flex-col gap-4">
-          {rows.map(({ label, values }, i) => (
-            <div key={i} className="flex gap-4 items-center">
-              <div className="w-24 text-sm text-gray-600 font-medium">
-                {label}:
-              </div>
-              <div className="flex gap-4 justify-center">
-                {values.map((name) => {
-                  const value = model.cells[name];
-                  return (
-                    <div key={name}>
-                      {value === "?" && (
-                        <div className="absolute top-1 right-2 text-gray-500">
-                          ?
+    <div className="flex flex-col h-full overflow-hidden">
+      <div className="flex-1 overflow-y-auto">
+        <div className="p-4 border-b">
+          <h2 className="text-lg font-semibold mb-2">Model</h2>
+          <div className="flex flex-col gap-4">
+            {rows.map(({ label, values }, i) => (
+              <div key={i} className="flex gap-4 items-center">
+                <div className="w-24 text-sm text-gray-600 font-medium">
+                  {label}:
+                </div>
+                <div className="flex gap-4 justify-center">
+                  {values.map((name) => {
+                    const value = model.cells[name];
+                    return (
+                      <div key={name}>
+                        {value === "?" && (
+                          <div className="absolute top-1 right-2 text-gray-500">
+                            ?
+                          </div>
+                        )}
+                        <div className="text-sm text-gray-600 font-mono">
+                          {name}
                         </div>
-                      )}
-                      <div className="text-sm text-gray-600 font-mono">
-                        {name}
-                      </div>
-                      <div
-                        className={`text-lg p-3 ${
-                          value === "?" ? "bg-gray-100" : "border"
-                        } rounded relative`}
-                      >
-                        {(() => {
-                          const ambValue = scenariosRef.current.map(
-                            (scenario) => scenario[name]
-                          );
-                          const filteredValues = ambValue.map((v) => ({
-                            value: v,
-                            include: true,
-                          }));
-                          const viewer = valueViewers.find(
-                            (v) => v.shouldRender(filteredValues) !== "hide"
-                          );
-                          if (viewer) {
-                            return viewer.component({ values: filteredValues });
-                          } else {
-                            return "noviewer";
-                          }
-                        })()}
-                        {/* return <CardViewer card={cardValue} />;
-                          } else if (cardValue instanceof PokerHand) {
-                            return <HandViewer hand={cardValue} />;
-                          }
+                        <div
+                          className={`text-lg p-3 ${
+                            value === "?" ? "bg-gray-100" : "border"
+                          } rounded relative`}
+                        >
+                          {(() => {
+                            const ambValue = scenariosRef.current.map(
+                              (scenario) => scenario[name]
+                            );
+                            const filteredValues = ambValue.map((v) => ({
+                              value: v,
+                              include: true,
+                            }));
+                            const viewer = valueViewers.find(
+                              (v) => v.shouldRender(filteredValues) !== "hide"
+                            );
+                            if (viewer) {
+                              return viewer.component({
+                                values: filteredValues,
+                              });
+                            } else {
+                              return "noviewer";
+                            }
+                          })()}
+                          {/* return <CardViewer card={cardValue} />;
+                            } else if (cardValue instanceof PokerHand) {
+                              return <HandViewer hand={cardValue} />;
+                            }
 
-                          return cardValue; */}
-                        {/* })()} */}
+                            return cardValue; */}
+                          {/* })()} */}
+                        </div>
                       </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="p-4">
+          <h2 className="text-lg font-semibold mb-2">Pick a scenario</h2>
+          <div className="mt-4">
+            <input
+              type="range"
+              min={0}
+              max={Math.max(0, scenarioCount - 1)}
+              value={selectedScenarioIndex}
+              onChange={(e) =>
+                setSelectedScenarioIndex(parseInt(e.target.value))
+              }
+              className="w-full"
+            />
+            <div className="text-sm text-gray-600">
+              Viewing scenario {selectedScenarioIndex + 1} of {scenarioCount}
+            </div>
+            <div>
+              {(() => {
+                if (!scenariosRef.current) {
+                  return <div></div>;
+                }
+
+                return [...handTypeCounts.entries()].map(([type, count]) => {
+                  return (
+                    <div>
+                      {type} {((100 * count) / scenarioCount).toFixed(2)}%
                     </div>
                   );
-                })}
-              </div>
+                });
+              })()}
             </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="p-4">
-        <h2 className="text-lg font-semibold mb-2">Pick a scenario</h2>
-        <div className="mt-4">
-          <input
-            type="range"
-            min={0}
-            max={Math.max(0, scenarioCount - 1)}
-            value={selectedScenarioIndex}
-            onChange={(e) => setSelectedScenarioIndex(parseInt(e.target.value))}
-            className="w-full"
-          />
-          <div className="text-sm text-gray-600">
-            Viewing scenario {selectedScenarioIndex + 1} of {scenarioCount}
-          </div>
-          <div>
-            {(() => {
-              if (!scenariosRef.current) {
-                return <div></div>;
-              }
-
-              return [...handTypeCounts.entries()].map(([type, count]) => {
-                return (
-                  <div>
-                    {type} {((100 * count) / scenarioCount).toFixed(2)}%
-                  </div>
-                );
-              });
-            })()}
           </div>
         </div>
-      </div>
 
-      <div className="p-4 text-xs text-gray-500">
-        {scenarioCount} scenarios enumerated in{" "}
-        {totalComputeTime.current.toFixed(0)}ms of active compute time. (
-        {((scenarioCount / totalComputeTime.current) * 1000).toFixed(0)}{" "}
-        scenarios/s)
-      </div>
+        <div className="p-4 text-xs text-gray-500">
+          {scenarioCount} scenarios enumerated in{" "}
+          {totalComputeTime.current.toFixed(0)}ms of active compute time. (
+          {((scenarioCount / totalComputeTime.current) * 1000).toFixed(0)}{" "}
+          scenarios/s)
+        </div>
 
-      <div className="p-4 text-xs">
-        <div>Scenario count limit: {maxScenarios}</div>
-        <Button
-          onClick={() => {
-            setMaxScenarios(maxScenarios * 2);
-          }}
-          variant="default"
-        >
-          Raise limit to {maxScenarios * 2}
-        </Button>
+        <div className="p-4 text-xs">
+          <div>Scenario count limit: {maxScenarios}</div>
+          <Button
+            onClick={() => {
+              setMaxScenarios(maxScenarios * 2);
+            }}
+            variant="default"
+          >
+            Raise limit to {maxScenarios * 2}
+          </Button>
+        </div>
       </div>
     </div>
   );
