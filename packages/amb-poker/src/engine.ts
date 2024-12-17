a7ccd279ad778f9f96d4ba3053947ab04adee837
Alessandro Warth <alexwarth@gmail.com>
2024-12-17 00:26:09 -0800

    add normal distribution from d3

--- packages/amb-poker/src/engine.ts
+++ packages/amb-poker/src/engine.ts
@@ -1,6 +1,7 @@
 import { Model, Scenario, isCard, allCards, Value, Card } from "./model";
 import { compileCell } from "./compiler";
 import { bestHand, cardRank, PokerHand } from "./handEvaluation";
+import * as d3 from "d3";
 
 type CompiledModel = {
   cells: {
@@ -209,12 +210,7 @@ export class Engine {
 
   // distributions
 
-  // taken from https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
   $normal(mean = 0, stdev = 1) {
-    const u = 1 - Math.random(); // convert [0,1) to (0,1]
-    const v = Math.random();
-    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-    // transform to the desired mean and standard deviation
-    return z * stdev + mean;
+    return d3.randomNormal(mean, stdev)();
   }
 }
