752a13d31f8a06bdd93f92a795cf57f0b0ef0eb4
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-11-04 14:35:59 +0100

    delete wasm plugin

--- tools/sync-indicator/vite.config.ts
+++ tools/sync-indicator/vite.config.ts
@@ -1,19 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
-import wasm from "vite-plugin-wasm";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "./",
-  plugins: [
-    topLevelAwait(),
-    wasm(),
-    react(),
-    tailwindcss(),
-    cssInjectedByJsPlugin(),
-  ],
+  plugins: [topLevelAwait(), react(), tailwindcss(), cssInjectedByJsPlugin()],
   build: {
     rollupOptions: {
       external(id) {
