dac9c1b2c8e4f6cb71f917679d670320313214ed
chee <yay@chee.party>
2025-11-26 20:22:17 +0000

    maybe tenfolder

--- core/filesystem/src/index.ts
+++ core/filesystem/src/index.ts
@@ -1,5 +1,46 @@
+import { type DocHandle, type Repo } from "@automerge/automerge-repo";
+import type { FolderDoc } from "./types.js";
+
 export * from "./metadata.js";
 export * from "./module-watcher.js";
 export * from "./packages.js";
 export * from "./sw.js";
 export type * from "./types.js";
+
+/**
+ *
+ * @param repo an Automerge Repo
+ * @param folderHandle the folder handle
+ * @param parts the path.split("/")
+
+ */
+export async function findHandleInFolderHandle<T>(
+  repo: Repo,
+  folderHandle: DocHandle<FolderDoc>,
+  parts: string[]
+) {
+  if (!parts.length) return folderHandle;
+  const part = parts[0];
+  if (!part) return folderHandle;
+  const folder = folderHandle.doc();
+  if (!folder.docs) return;
+
+  const docLink = folder.docs.find((doc) => doc.name === part);
+  if (!docLink) return;
+
+  const docHandle = await repo.find(docLink.url);
+
+  if (parts.length > 1) {
+    const doc = docHandle.doc();
+    if (!("docs" in doc)) {
+      return;
+    }
+    return findHandleInFolderHandle(
+      repo,
+      docHandle as DocHandle<FolderDoc>,
+      parts.slice(1)
+    );
+  }
+  // todo kind of a lie
+  return docHandle as DocHandle<T>;
+}
