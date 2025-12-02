90d046e9d5040531240e8fa93cda45fc78644835
chee <yay@chee.party>
2025-12-02 12:57:18 +0000

    actually build the service worker

--- core/bootloader/src/vite/service-worker-plugin.ts
+++ core/bootloader/src/vite/service-worker-plugin.ts
@@ -1,12 +1,39 @@
 import { type Plugin, transformWithEsbuild } from "vite";
+import { type ResolvedId, type RollupFsModule } from "rollup";
 
 export function serviceworker(): Plugin {
   const moduleId = "service-worker.js";
   const path = `/${moduleId}`;
   const ids = [moduleId, path];
   const serviceWorkerExport = "@patchwork/bootloader/service-worker";
+  async function transform(
+    resolve: (source: string) => Promise<ResolvedId | null>,
+    fs: RollupFsModule
+  ) {
+    const exportPath = await resolve(serviceWorkerExport);
+    const file = await fs.readFile(exportPath!.id, {
+      encoding: "utf8",
+    });
+    const transformation = await transformWithEsbuild(
+      file,
+      serviceWorkerExport,
+      { format: "iife" }
+    );
+    return transformation;
+  }
   return {
     name: "@patchwork/vite",
+    async buildStart() {
+      if (this.environment.mode == "build") {
+        const trans = await transform(this.resolve, this.fs);
+        this.emitFile({
+          type: "prebuilt-chunk",
+          fileName: path.slice(1),
+          code: trans.code,
+          map: trans.map,
+        });
+      }
+    },
     resolveId(id) {
       if (ids.includes(id)) {
         return moduleId;
@@ -14,16 +41,7 @@ export function serviceworker(): Plugin {
     },
     async load(id) {
       if (ids.includes(id)) {
-        const exportPath = await this.resolve(serviceWorkerExport);
-        const file = await this.fs.readFile(exportPath!.id, {
-          encoding: "utf8",
-        });
-        const transformation = await transformWithEsbuild(
-          file,
-          serviceWorkerExport,
-          { format: "iife" }
-        );
-        return transformation;
+        return transform(this.resolve, this.fs);
       }
     },
   };
