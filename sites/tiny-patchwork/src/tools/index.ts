59c33d7a3eae2baaf1c9c7c8130d9ad6c6fa5fdb
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-12-10 15:58:22 +0100

    delete todo tool from tiny-patchwork

--- sites/tiny-patchwork/src/tools/index.ts
+++ sites/tiny-patchwork/src/tools/index.ts
@@ -6,8 +6,6 @@ import { plugins as tabViewerPlugins } from "@tiny-patchwork/context-sidebar";
 // @ts-expect-error no types
 import { plugins as branchViewPlugins } from "@tiny-patchwork/history-view";
 // @ts-expect-error no types
-import { plugins as todoPlugins } from "@tiny-patchwork/todo";
-// @ts-expect-error no types
 import { plugins as historyViewPlugins } from "@tiny-patchwork/history-view";
 // @ts-expect-error no types
 import { plugins as commentsViewPlugins } from "@tiny-patchwork/comments-view";
@@ -62,7 +60,6 @@ export const plugins: Plugin<any>[] = [
   ...patchworkFramePlugins,
   ...tabViewerPlugins,
   ...branchViewPlugins,
-  ...todoPlugins,
   ...historyViewPlugins,
   ...commentsViewPlugins,
   ...sideboardPlugins,
