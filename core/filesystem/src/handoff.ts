d54ab222ea523ea19f32db9ec9d6b36682cc25a8
chee <yay@chee.party>
2026-02-13 12:17:20 +0000

    Apply suggestions from code review

--- core/filesystem/src/handoff.ts
+++ core/filesystem/src/handoff.ts
@@ -127,7 +127,7 @@ export function createFilesystemHandoffHandler(repo: Repo) {
           );
         }
 
-        if (!isPackageJsonRequest) {
+        if (!isPackageJsonRequest && path.length) {
           const key = maybeAutomergeUrl;
           if (!(key in loaders)) {
             const pkg = await packageJsonContentsFromFolderDocUrl(key);
