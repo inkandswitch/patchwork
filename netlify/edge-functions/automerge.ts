4bab70072bae8f4b40c53a16b6c022458947b57e
chee <yay@chee.party>
2026-03-04 13:14:44 +0000

    fix suffix

--- netlify/edge-functions/automerge.ts
+++ netlify/edge-functions/automerge.ts
@@ -1,4 +1,4 @@
-import { automergeWasmBase64 } from "@automerge/automerge/automerge.wasm.base64.js";
+import { automergeWasmBase64 } from "@automerge/automerge/automerge.wasm.base64";
 import {
   initializeBase64Wasm,
   Repo,
