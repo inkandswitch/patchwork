c232b639170a5cfb75a8488158e68cedbe58d7a1
grjte <grjte@protonmail.com>
2026-05-22 15:28:24 +0100

    feat: add placeholder plumbing for resource restrictions

--- core/elements/src/index.ts
+++ core/elements/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./patchwork-view.js";
 export * from "./isolated-patchwork-view/index.js";
+export * from "./isolated-patchwork-view/resource-policy.js";
 export * from "./events.js";
