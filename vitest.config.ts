6e50616c2f45dd205f03a82d30f5bd746d2e898c
chee <yay@chee.party>
2026-07-18 15:04:54 +0200

    remove packages that dont exist

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   test: {
     workspace: [
       "core/filesystem",
-      "packages/refs",
-      "packages/annotations/core",
-      "packages/annotations/context",
       "packages/edge-handles",
     ],
   },
